5941a5db9455587cd36ff41c40656a54a147d6b7
Gregg Van Hove <pair+gvanhove@pivotallabs.com>
2013-05-24 13:49:20 -0700

    mock message bus can respond to requests

--- lib/cf_message_bus/mock_message_bus.rb
+++ lib/cf_message_bus/mock_message_bus.rb
@@ -3,6 +3,7 @@ module CfMessageBus
     def initialize(config = {})
       @logger = config[:logger]
       @subscriptions = Hash.new([])
+      @requests = {}
     end
 
     def subscribe(subject, opts = {}, &blk)
@@ -11,11 +12,27 @@ module CfMessageBus
 
     def publish(subject, message = nil)
       @subscriptions[subject].each do |subscription|
-        subscription.call(message)
+        subscription.call(symbolize_keys(message))
       end
     end
 
-    def request(subject, data=nil, opts={})
+    def request(subject, data=nil, opts={}, &blk)
+      @requests[subject] = blk
+    end
+
+    def respond_to_request(request_subject, data)
+      block = @requests.fetch(request_subject) { raise "No request for #{request_subject}" }
+      block.call(symbolize_keys(data))
+    end
+
+    private
+
+    def symbolize_keys(hash)
+      return hash unless hash.is_a?(Hash)
+      hash.inject({}) do |memo, (key, value)|
+        memo[key.to_sym] = symbolize_keys(value)
+        memo
+      end
     end
   end
 end
--- spec/mock_message_bus_spec.rb
+++ spec/mock_message_bus_spec.rb
@@ -5,17 +5,53 @@ module CfMessageBus
   describe MockMessageBus do
     it_behaves_like :a_message_bus
 
-    it 'should call subscribers inline' do
-      publish_data = {:foo => 'bar', :baz => 'quux'}
-      bus = MockMessageBus.new({})
-      bus.subscribe("foo") do |data|
-        expect(data).to eql(publish_data)
-        raise "called callback"
-      end
+    let(:bus) { MockMessageBus.new }
 
-      expect {
-        bus.publish("foo", publish_data)
-      }.to raise_error(/called callback/)
+    it 'should call subscribers inline' do
+      received_data = nil
+
+      bus.subscribe("foo") do |data|
+        received_data = data
+      end
+      expect(received_data).to be_nil
+
+      publish_data = 'bar'
+      bus.publish("foo", publish_data)
+      expect(received_data).to eql(publish_data)
+    end
+
+    it 'should symbolize keys to the subscriber' do
+      received_data = nil
+
+      bus.subscribe("foo") do |data|
+        received_data = data
+      end
+      expect(received_data).to be_nil
+
+      bus.publish("foo",  { 'foo' => 'bar', 'baz' => 'quux' })
+      expect(received_data).to eql({ foo: 'bar', baz: 'quux' })
+    end
+
+    it 'should respond to requests' do
+      received_data = nil
+      bus.request('hey guys') do |data|
+        received_data = data
+      end
+      expect(received_data).to be_nil
+
+      bus.respond_to_request('hey guys', 'foo')
+      expect(received_data).to eql('foo')
+    end
+
+    it 'should symbolize keys when responding to requests' do
+      received_data = nil
+      bus.request('hey guys') do |data|
+        received_data = data
+      end
+      expect(received_data).to be_nil
+
+      bus.respond_to_request('hey guys', { 'foo' => 'bar' })
+      expect(received_data).to eql({ foo: 'bar' })
     end
   end
 end
